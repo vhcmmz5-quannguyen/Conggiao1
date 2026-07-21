687f7acac86f3d4f2ece739a840271bef4b96f96
vhcmmz5-quannguyen <vhcmmz5@gmail.com>
2026-07-21 13:45:48 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+function isOldAppCache(name) {
+  return name === 'app-quantri-v7-beta' || name === 'app-quantri-v8-pwa-icons';
+}
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    try {
+      const keys = await caches.keys();
+      await Promise.allSettled(keys.filter(isOldAppCache).map(name => caches.delete(name)));
+      await self.clients.claim();
+      const clientsList = await self.clients.matchAll({ type: 'window' });
+      await Promise.allSettled(clientsList.map(client => client.navigate(client.url)));
+    } finally {
+      await self.registration.unregister();
+    }
+  })());
+});
